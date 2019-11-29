0cbc4bd7f075ead94f521b6724a43b497d6842b1
ghazikr <ghazikadardar@gmail.com>
2019-11-29 22:33:21 +0100

    added stepper to add activity

--- client/src/components/activities/add_activity.js
+++ client/src/components/activities/add_activity.js
@@ -5,8 +5,10 @@ import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
 import Select from "@material-ui/core/Select";
+import Stepper from "@material-ui/core/Stepper";
+import Step from "@material-ui/core/Step";
+import StepLabel from "@material-ui/core/StepLabel";
 import FormControl from "@material-ui/core/FormControl";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import InputLabel from "@material-ui/core/InputLabel";
@@ -27,7 +29,66 @@ function AddActivity(props) {
     handleSubmit,
     closeAddActivityDialog
   } = props;
+
   const classes = useStyles();
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  function getStepContent(step) {
+    switch (step) {
+      case 0:
+        return (
+          <Field
+            name="category"
+            component={renderSelectField}
+            label="Category Type"
+          >
+            {props.categories.map(category => (
+              <option key={category.iconName} value={category.iconName}>
+                {category.name}
+              </option>
+            ))}
+          </Field>
+        );
+      case 1:
+        return (
+          <>
+            <Field
+              name="activityType"
+              component={renderSelectField}
+              label="Activity Type"
+            >
+              <option value="expense">Expense</option>
+              <option value="income">Income</option>
+            </Field>
+            <Field
+              name="name"
+              type="text"
+              placeholder="Name"
+              component={renderTextField}
+              autoComplete="none"
+            />
+
+            <Field
+              id="date"
+              name="date"
+              placeholder="Date"
+              component={renderCustomDatePicker}
+            />
+
+            <Field
+              id="amount"
+              name="amount"
+              type="number"
+              placeholder="Amount"
+              component={renderTextField}
+              autoComplete="none"
+            />
+          </>
+        );
+      default:
+        return "Unknown step";
+    }
+  }
 
   const renderFromHelper = ({ touched, error }) => {
     if (!(touched && error)) {
@@ -98,65 +159,48 @@ function AddActivity(props) {
       />
     </MuiPickersUtilsProvider>
   );
+  const steps = ["Choose a category", "Create your activity"];
+
+  const handleBack = () => {
+    setActiveStep(prevActiveStep => prevActiveStep - 1);
+  };
+
+  const handleNext = () => {
+    setActiveStep(prevActiveStep => prevActiveStep + 1);
+    if (activeStep === 1) {
+      setActiveStep(0);
+      closeAddActivityDialog();
+      handleSubmit(onSubmit)();
+    }
+  };
   return (
     <Dialog
       open={isActivityDialogOpen}
       onClose={handleDialogState}
       aria-labelledby="form-dialog-title"
     >
-      <DialogTitle id="form-dialog-title">{`${props.dialogTitle} an activity`}</DialogTitle>
+      <DialogTitle id="form-dialog-title">
+        <Stepper activeStep={activeStep}>
+          {steps.map((label, index) => {
+            const stepProps = {};
+            const labelProps = {};
+            return (
+              <Step key={label} {...stepProps}>
+                <StepLabel {...labelProps}>{label}</StepLabel>
+              </Step>
+            );
+          })}
+        </Stepper>
+      </DialogTitle>
       <DialogContent>
-        <DialogContentText>
-          Please add the details of your activity
-        </DialogContentText>
+        <div>
+          <div>{getStepContent(activeStep)}</div>
+        </div>
         <form
           className={classes.form}
           noValidate
           onSubmit={handleSubmit(onSubmit)}
         >
-          <Field
-            name="activityType"
-            component={renderSelectField}
-            label="Activity Type"
-          >
-            <option value="expense">Expense</option>
-            <option value="income">Income</option>
-          </Field>
-          <Field
-            name="name"
-            type="text"
-            placeholder="Name"
-            component={renderTextField}
-            autoComplete="none"
-          />
-
-          <Field
-            name="category"
-            component={renderSelectField}
-            label="Category Type"
-          >
-            {props.categories.map(category => (
-              <option key={category.iconName} value={category.iconName}>
-                {category.name}
-              </option>
-            ))}
-          </Field>
-
-          <Field
-            id="date"
-            name="date"
-            placeholder="Date"
-            component={renderCustomDatePicker}
-          />
-
-          <Field
-            id="amount"
-            name="amount"
-            type="number"
-            placeholder="Amount"
-            component={renderTextField}
-            autoComplete="none"
-          />
           <div>{errorMessage}</div>
         </form>
       </DialogContent>
@@ -164,13 +208,25 @@ function AddActivity(props) {
         <Button onClick={closeAddActivityDialog} color="primary">
           Cancel
         </Button>
+        {activeStep !== 0 && (
+          <>
+            <Button
+              disabled={activeStep === 0}
+              onClick={handleBack}
+              className={classes.button}
+            >
+              Back
+            </Button>
+          </>
+        )}
+
         <Button
-          type="submit"
           variant="contained"
           color="primary"
-          onClick={handleSubmit(onSubmit)}
+          onClick={handleNext}
+          className={classes.button}
         >
-          Save
+          {activeStep === steps.length - 1 ? "Finish" : "Next"}
         </Button>
       </DialogActions>
     </Dialog>
