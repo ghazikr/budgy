121961ee0aea4333adf2622fa5d2c3b3e94c53a9
ghazikr <ghazikadardar@gmail.com>
2020-02-18 21:02:05 +0100

    fix dep again

---
+++ server/config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID:
+    "162595762574-j76cdhl0pkdg7f1aclav8s5d7ajbcsev.apps.googleusercontent.com",
+  googleSecretID: "VXvcIZ4LbAoibPYo_ycBC_BN",
+  mongoURI: "mongodb://ghazi:ghazi123@ds035735.mlab.com:35735/budgy-db",
+  cookieKey: "regrhrtjolfdefrehrh",
+  //jwt
+  secret: "gdjhgkdglydgld"
+};
