716d3a0d383cff395ff4321854c717001740befe
ghazikr <ghazikadardar@gmail.com>
2020-02-18 22:01:09 +0100

    added proxy + urls fixes

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,9 +1,5 @@
-module.exports = {
-  googleClientID:
-    "162595762574-j76cdhl0pkdg7f1aclav8s5d7ajbcsev.apps.googleusercontent.com",
-  googleSecretID: "VXvcIZ4LbAoibPYo_ycBC_BN",
-  mongoURI: "mongodb://ghazi:ghazi123@ds035735.mlab.com:35735/budgy-db",
-  cookieKey: "regrhrtjolfdefrehrh",
-  //jwt
-  secret: "gdjhgkdglydgld"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./prod");
+} else {
+  module.exports = require("./dev");
+}
